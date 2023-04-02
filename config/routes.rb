ff00e2b77bcd6907ee837b252a83029f6aa40755
Mark Bowen <mbowen@uccs.edu>
2023-04-02 20:47:50 +0000

    Rails setup 0.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :eggs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "objects#index"
+  root "eggs#index"
+  resources :eggs
+  
 end
